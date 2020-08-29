15e0357e8093145c12566fbea8b0309da1d661f0
santoshsaha84 <santosh_saha84@yahoo.co.in>
2020-08-29 17:20:58 +0530

    DOT NET CORE AZURE DEVOPS WITH YAML FILE

---
+++ SeleniumDemoYAML/SearchGoogle.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace SeleniumDemo
+{
+    [TestClass]
+    public class SearchGoogle
+    {
+
+        [TestMethod]
+        public void SearchForCheese()
+        {
+            
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("headless");
+            var  driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),chromeOptions);
+
+            driver.Navigate().GoToUrl("https://google.com");
+            System.Threading.Thread.Sleep(3000);
+            driver.FindElement(By.Name("q")).SendKeys("Hello World");
+            System.Threading.Thread.Sleep(3000);
+            driver.FindElement(By.Name("btnK")).SendKeys(Keys.Return);
+
+            driver.Close();
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
